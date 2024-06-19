88534d6aa2c6e540b588c8f6c3f087844db13467
chusevip8 <chusevip8@gmail.com>
2024-06-19 15:22:46 +0800

    完善功能。

--- web/src/view/octopus/commentTask/commentTask.vue
+++ web/src/view/octopus/commentTask/commentTask.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <div class="gva-search-box">
-      <el-form ref="elSearchFormRef" :inline="true" :model="searchInfo" class="demo-form-inline" :rules="searchRule" @keyup.enter="onSubmit">
-      <el-form-item label="创建日期" prop="createdAt">
+      <el-form ref="elSearchFormRef" :inline="true" :model="searchInfo" class="demo-form-inline" :rules="searchRule"
+        @keyup.enter="onSubmit">
+        <!-- <el-form-item label="创建日期" prop="createdAt">
       <template #label>
         <span>
           创建日期
@@ -11,17 +12,19 @@
           </el-tooltip>
         </span>
       </template>
-      <el-date-picker v-model="searchInfo.startCreatedAt" type="datetime" placeholder="开始日期" :disabled-date="time=> searchInfo.endCreatedAt ? time.getTime() > searchInfo.endCreatedAt.getTime() : false"></el-date-picker>
-       —
-      <el-date-picker v-model="searchInfo.endCreatedAt" type="datetime" placeholder="结束日期" :disabled-date="time=> searchInfo.startCreatedAt ? time.getTime() < searchInfo.startCreatedAt.getTime() : false"></el-date-picker>
-      </el-form-item>
-      
+<el-date-picker v-model="searchInfo.startCreatedAt" type="datetime" placeholder="开始日期"
+  :disabled-date="time=> searchInfo.endCreatedAt ? time.getTime() > searchInfo.endCreatedAt.getTime() : false"></el-date-picker>
+—
+<el-date-picker v-model="searchInfo.endCreatedAt" type="datetime" placeholder="结束日期"
+  :disabled-date="time=> searchInfo.startCreatedAt ? time.getTime() < searchInfo.startCreatedAt.getTime() : false"></el-date-picker>
+</el-form-item> -->
+
         <el-form-item label="任务标题" prop="title">
-         <el-input v-model="searchInfo.title" placeholder="搜索条件" />
+          <el-input v-model="searchInfo.title" placeholder="搜索条件" />
 
         </el-form-item>
         <el-form-item label="评论关键字" prop="keyword">
-         <el-input v-model="searchInfo.keyword" placeholder="搜索条件" />
+          <el-input v-model="searchInfo.keyword" placeholder="搜索条件" />
 
         </el-form-item>
 
@@ -32,74 +35,61 @@
         <el-form-item>
           <el-button type="primary" icon="search" @click="onSubmit">查询</el-button>
           <el-button icon="refresh" @click="onReset">重置</el-button>
-          <el-button type="text" icon="arrow-down" @click="showAllQuery=true" v-if="!showAllQuery">展开</el-button>
-          <el-button type="text" icon="arrow-up" @click="showAllQuery=false" v-else>收起</el-button>
+          <el-button type="text" icon="arrow-down" @click="showAllQuery = true" v-if="!showAllQuery">展开</el-button>
+          <el-button type="text" icon="arrow-up" @click="showAllQuery = false" v-else>收起</el-button>
         </el-form-item>
       </el-form>
     </div>
     <div class="gva-table-box">
-        <div class="gva-btn-list">
-            <el-button type="primary" icon="plus" @click="openDialog">新增</el-button>
-            <el-button icon="delete" style="margin-left: 10px;" :disabled="!multipleSelection.length" @click="onDelete">删除</el-button>
-        </div>
-        <el-table
-        ref="multipleTable"
-        style="width: 100%"
-        tooltip-effect="dark"
-        :data="tableData"
-        row-key="ID"
-        @selection-change="handleSelectionChange"
-        >
+      <div class="gva-btn-list">
+        <el-button type="primary" icon="plus" @click="openDialog">新增</el-button>
+        <el-button icon="delete" style="margin-left: 10px;" :disabled="!multipleSelection.length"
+          @click="onDelete">删除</el-button>
+      </div>
+      <el-table ref="multipleTable" style="width: 100%" tooltip-effect="dark" :data="tableData" row-key="ID"
+        @selection-change="handleSelectionChange">
         <el-table-column type="selection" width="55" />
-        
-        <el-table-column align="left" label="日期" prop="createdAt" width="180">
-            <template #default="scope">{{ formatDate(scope.row.CreatedAt) }}</template>
-        </el-table-column>
-        
-        <el-table-column align="left" label="任务标题" prop="title" width="120" />
-        <el-table-column align="left" label="文章ID" prop="articleID" width="120" />
-        <el-table-column align="left" label="评论关键字" prop="keyword" width="120" />
-        <el-table-column align="left" label="操作" fixed="right" min-width="240">
-            <template #default="scope">
-            <el-button type="primary" link icon="edit" class="table-button" @click="updateCommentTaskFunc(scope.row)">变更</el-button>
+
+        <el-table-column align="center" label="任务标题" prop="title" width="180" />
+        <el-table-column align="center" label="文章ID" prop="articleID" width="180" />
+        <el-table-column align="center" label="评论关键字" prop="keyword" min-width="240" />
+        <el-table-column align="center" label="操作" fixed="right" min-width="240">
+          <template #default="scope">
+            <el-button type="primary" link icon="Cellphone" class="table-button">管理设备</el-button>
+            <el-button type="primary" link icon="edit" class="table-button"
+              @click="updateCommentTaskFunc(scope.row)">修改</el-button>
             <el-button type="primary" link icon="delete" @click="deleteRow(scope.row)">删除</el-button>
-            </template>
+          </template>
         </el-table-column>
-        </el-table>
-        <div class="gva-pagination">
-            <el-pagination
-            layout="total, sizes, prev, pager, next, jumper"
-            :current-page="page"
-            :page-size="pageSize"
-            :page-sizes="[10, 30, 50, 100]"
-            :total="total"
-            @current-change="handleCurrentChange"
-            @size-change="handleSizeChange"
-            />
-        </div>
+      </el-table>
+      <div class="gva-pagination">
+        <el-pagination layout="total, sizes, prev, pager, next, jumper" :current-page="page" :page-size="pageSize"
+          :page-sizes="[10, 30, 50, 100]" :total="total" @current-change="handleCurrentChange"
+          @size-change="handleSizeChange" />
+      </div>
     </div>
     <el-drawer destroy-on-close size="800" v-model="dialogFormVisible" :show-close="false" :before-close="closeDialog">
-       <template #header>
-              <div class="flex justify-between items-center">
-                <span class="text-lg">{{type==='create'?'添加':'修改'}}</span>
-                <div>
-                  <el-button type="primary" @click="enterDialog">确 定</el-button>
-                  <el-button @click="closeDialog">取 消</el-button>
-                </div>
-              </div>
-            </template>
+      <template #header>
+        <div class="flex justify-between items-center">
+          <span class="text-lg">{{ type === 'create' ? '添加' : '修改' }}</span>
+          <div>
+            <el-button type="primary" @click="enterDialog">确 定</el-button>
+            <el-button @click="closeDialog">取 消</el-button>
+          </div>
+        </div>
+      </template>
 
-          <el-form :model="formData" label-position="top" ref="elFormRef" :rules="rule" label-width="80px">
-            <el-form-item label="任务标题:"  prop="title" >
-              <el-input v-model="formData.title" :clearable="false"  placeholder="请输入任务标题" />
-            </el-form-item>
-            <el-form-item label="文章ID:"  prop="articleID" >
-              <el-input v-model="formData.articleID" :clearable="false"  placeholder="请输入文章ID" />
-            </el-form-item>
-            <el-form-item label="评论关键字:"  prop="keyword" >
-              <el-input v-model="formData.keyword" :clearable="false"  placeholder="请输入评论关键字" />
-            </el-form-item>
-          </el-form>
+      <el-form :model="formData" label-position="top" ref="elFormRef" :rules="rule" label-width="80px">
+        <el-form-item label="任务标题:" prop="title">
+          <el-input v-model="formData.title" :clearable="false" placeholder="请输入任务标题" />
+        </el-form-item>
+        <el-form-item label="文章ID:" prop="articleID">
+          <el-input v-model="formData.articleID" :clearable="false" placeholder="请输入文章ID" />
+        </el-form-item>
+        <el-form-item label="评论关键字:（以半角逗号分隔）" prop="keyword">
+          <el-input v-model="formData.keyword" :clearable="false" placeholder="请输入评论关键字" />
+        </el-form-item>
+      </el-form>
     </el-drawer>
   </div>
 </template>
@@ -115,65 +105,73 @@ import {
 } from '@/api/octopus/commentTask'
 
 // 全量引入格式化工具 请按需保留
-import { getDictFunc, formatDate, formatBoolean, filterDict ,filterDataSource, ReturnArrImg, onDownloadFile } from '@/utils/format'
+import { getDictFunc, formatDate, formatBoolean, filterDict, filterDataSource, ReturnArrImg, onDownloadFile } from '@/utils/format'
 import { ElMessage, ElMessageBox } from 'element-plus'
 import { ref, reactive } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
 
 defineOptions({
-    name: 'CommentTask'
+  name: 'CommentTask'
 })
+
+const router = useRouter()
+const route = useRoute()
 
 // 控制更多查询条件显示/隐藏状态
 const showAllQuery = ref(false)
 
 // 自动化生成的字典（可能为空）以及字段
 const formData = ref({
-        title: '',
-        articleID: '',
-        keyword: '',
-        })
+  appName: '',
+  title: '',
+  articleID: '',
+  keyword: '',
+  scriptID: 0,
+})
 
 
 
 // 验证规则
 const rule = reactive({
-               title : [{
-                   required: true,
-                   message: '任务标题不能为空',
-                   trigger: ['input','blur'],
-               },
-               {
-                   whitespace: true,
-                   message: '不能只输入空格',
-                   trigger: ['input', 'blur'],
-              }
-              ],
-               articleID : [{
-                   required: true,
-                   message: '文章ID不能为空',
-                   trigger: ['input','blur'],
-               },
-               {
-                   whitespace: true,
-                   message: '不能只输入空格',
-                   trigger: ['input', 'blur'],
-              }
-              ],
+  title: [{
+    required: true,
+    message: '任务标题不能为空',
+    trigger: ['input', 'blur'],
+  },
+  {
+    whitespace: true,
+    message: '不能只输入空格',
+    trigger: ['input', 'blur'],
+  }
+  ],
+  articleID: [{
+    required: true,
+    message: '文章ID不能为空',
+    trigger: ['input', 'blur'],
+  },
+  {
+    whitespace: true,
+    message: '不能只输入空格',
+    trigger: ['input', 'blur'],
+  }
+  ],
 })
 
 const searchRule = reactive({
   createdAt: [
-    { validator: (rule, value, callback) => {
-      if (searchInfo.value.startCreatedAt && !searchInfo.value.endCreatedAt) {
-        callback(new Error('请填写结束日期'))
-      } else if (!searchInfo.value.startCreatedAt && searchInfo.value.endCreatedAt) {
-        callback(new Error('请填写开始日期'))
-      } else if (searchInfo.value.startCreatedAt && searchInfo.value.endCreatedAt && (searchInfo.value.startCreatedAt.getTime() === searchInfo.value.endCreatedAt.getTime() || searchInfo.value.startCreatedAt.getTime() > searchInfo.value.endCreatedAt.getTime())) {
-        callback(new Error('开始日期应当早于结束日期'))
-      } else {
-        callback()
-      }
-    }, trigger: 'change' }
+    {
+      validator: (rule, value, callback) => {
+        if (searchInfo.value.startCreatedAt && !searchInfo.value.endCreatedAt) {
+          callback(new Error('请填写结束日期'))
+        } else if (!searchInfo.value.startCreatedAt && searchInfo.value.endCreatedAt) {
+          callback(new Error('请填写开始日期'))
+        } else if (searchInfo.value.startCreatedAt && searchInfo.value.endCreatedAt && (searchInfo.value.startCreatedAt.getTime() === searchInfo.value.endCreatedAt.getTime() || searchInfo.value.startCreatedAt.getTime() > searchInfo.value.endCreatedAt.getTime())) {
+          callback(new Error('开始日期应当早于结束日期'))
+        } else {
+          callback()
+        }
+      }, trigger: 'change'
+    }
   ],
 })
 
@@ -195,7 +193,7 @@ const onReset = () => {
 
 // 搜索
 const onSubmit = () => {
-  elSearchFormRef.value?.validate(async(valid) => {
+  elSearchFormRef.value?.validate(async (valid) => {
     if (!valid) return
     page.value = 1
     pageSize.value = 10
@@ -216,7 +214,7 @@ const handleCurrentChange = (val) => {
 }
 
 // 查询
-const getTableData = async() => {
+const getTableData = async () => {
   const table = await getCommentTaskList({ page: page.value, pageSize: pageSize.value, ...searchInfo.value })
   if (table.code === 0) {
     tableData.value = table.data.list
@@ -231,7 +229,7 @@ getTableData()
 // ============== 表格控制部分结束 ===============
 
 // 获取需要的字典 可能为空 按需保留
-const setOptions = async () =>{
+const setOptions = async () => {
 }
 
 // 获取需要的字典 可能为空 按需保留
@@ -242,80 +240,80 @@ setOptions()
 const multipleSelection = ref([])
 // 多选
 const handleSelectionChange = (val) => {
-    multipleSelection.value = val
+  multipleSelection.value = val
 }
 
 // 删除行
 const deleteRow = (row) => {
-    ElMessageBox.confirm('确定要删除吗?', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-    }).then(() => {
-            deleteCommentTaskFunc(row)
-        })
-    }
-
-// 多选删除
-const onDelete = async() => {
   ElMessageBox.confirm('确定要删除吗?', '提示', {
     confirmButtonText: '确定',
     cancelButtonText: '取消',
     type: 'warning'
-  }).then(async() => {
-      const IDs = []
-      if (multipleSelection.value.length === 0) {
-        ElMessage({
-          type: 'warning',
-          message: '请选择要删除的数据'
-        })
-        return
-      }
-      multipleSelection.value &&
-        multipleSelection.value.map(item => {
-          IDs.push(item.ID)
-        })
-      const res = await deleteCommentTaskByIds({ IDs })
-      if (res.code === 0) {
-        ElMessage({
-          type: 'success',
-          message: '删除成功'
-        })
-        if (tableData.value.length === IDs.length && page.value > 1) {
-          page.value--
-        }
-        getTableData()
-      }
+  }).then(() => {
+    deleteCommentTaskFunc(row)
+  })
+}
+
+// 多选删除
+const onDelete = async () => {
+  ElMessageBox.confirm('确定要删除吗?', '提示', {
+    confirmButtonText: '确定',
+    cancelButtonText: '取消',
+    type: 'warning'
+  }).then(async () => {
+    const IDs = []
+    if (multipleSelection.value.length === 0) {
+      ElMessage({
+        type: 'warning',
+        message: '请选择要删除的数据'
       })
+      return
     }
+    multipleSelection.value &&
+      multipleSelection.value.map(item => {
+        IDs.push(item.ID)
+      })
+    const res = await deleteCommentTaskByIds({ IDs })
+    if (res.code === 0) {
+      ElMessage({
+        type: 'success',
+        message: '删除成功'
+      })
+      if (tableData.value.length === IDs.length && page.value > 1) {
+        page.value--
+      }
+      getTableData()
+    }
+  })
+}
 
 // 行为控制标记（弹窗内部需要增还是改）
 const type = ref('')
 
 // 更新行
-const updateCommentTaskFunc = async(row) => {
-    const res = await findCommentTask({ ID: row.ID })
-    type.value = 'update'
-    if (res.code === 0) {
-        formData.value = res.data
-        dialogFormVisible.value = true
-    }
+const updateCommentTaskFunc = async (row) => {
+  const res = await findCommentTask({ ID: row.ID })
+  type.value = 'update'
+  if (res.code === 0) {
+    formData.value = res.data
+    dialogFormVisible.value = true
+  }
 }
 
 
 // 删除行
 const deleteCommentTaskFunc = async (row) => {
-    const res = await deleteCommentTask({ ID: row.ID })
-    if (res.code === 0) {
-        ElMessage({
-                type: 'success',
-                message: '删除成功'
-            })
-            if (tableData.value.length === 1 && page.value > 1) {
-            page.value--
-        }
-        getTableData()
+  const res = await deleteCommentTask({ ID: row.ID })
+  if (res.code === 0) {
+    ElMessage({
+      type: 'success',
+      message: '删除成功'
+    })
+    if (tableData.value.length === 1 && page.value > 1) {
+      page.value--
     }
+    getTableData()
+  }
 }
 
 // 弹窗控制标记
@@ -323,48 +321,50 @@ const dialogFormVisible = ref(false)
 
 // 打开弹窗
 const openDialog = () => {
-    type.value = 'create'
-    dialogFormVisible.value = true
+  type.value = 'create'
+  dialogFormVisible.value = true
 }
 
 // 关闭弹窗
 const closeDialog = () => {
-    dialogFormVisible.value = false
-    formData.value = {
-        title: '',
-        articleID: '',
-        keyword: '',
-        }
+  dialogFormVisible.value = false
+  formData.value = {
+    appName: '',
+    title: '',
+    articleID: '',
+    keyword: '',
+    scriptID: 0,
+  }
 }
 // 弹窗确定
 const enterDialog = async () => {
-     elFormRef.value?.validate( async (valid) => {
-             if (!valid) return
-              let res
-              switch (type.value) {
-                case 'create':
-                  res = await createCommentTask(formData.value)
-                  break
-                case 'update':
-                  res = await updateCommentTask(formData.value)
-                  break
-                default:
-                  res = await createCommentTask(formData.value)
-                  break
-              }
-              if (res.code === 0) {
-                ElMessage({
-                  type: 'success',
-                  message: '创建/更改成功'
-                })
-                closeDialog()
-                getTableData()
-              }
+  elFormRef.value?.validate(async (valid) => {
+    if (!valid) return
+    formData.value.appName = route.params.appName
+    formData.value.scriptID = parseInt(route.params.scriptId, 10)
+    let res
+    switch (type.value) {
+      case 'create':
+        res = await createCommentTask(formData.value)
+        break
+      case 'update':
+        res = await updateCommentTask(formData.value)
+        break
+      default:
+        res = await createCommentTask(formData.value)
+        break
+    }
+    if (res.code === 0) {
+      ElMessage({
+        type: 'success',
+        message: '创建/更改成功'
       })
+      closeDialog()
+      getTableData()
+    }
+  })
 }
 
 </script>
 
-<style>
-
-</style>
+<style></style>
