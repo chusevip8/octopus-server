671c67e0b8617eab042524d9b9067eb961b60e9c
chusevip8 <chusevip8@gmail.com>
2024-06-28 15:32:31 +0800

    完善功能。

--- server/service/octopus/cmt_msg_mgr.go
+++ server/service/octopus/cmt_msg_mgr.go
@@ -2,8 +2,6 @@ package octopus
 
 import (
 	"encoding/json"
-	"github.com/flipped-aurora/gin-vue-admin/server/global"
-	"github.com/flipped-aurora/gin-vue-admin/server/model/octopus"
 	"github.com/flipped-aurora/gin-vue-admin/server/model/octopus/request"
 )
 
@@ -12,7 +10,8 @@ type CmtMsgMgrService struct{}
 func (cmtMsgMgrService *CmtMsgMgrService) SaveCmtMeg(data string) (err error) {
 	var cmtMsg request.CmtMsg
 	err = json.Unmarshal([]byte(data), &cmtMsg)
-	if err == nil {
-		err = global.GVA_DB.Model(&octopus.Conversation{}).Where("taskID = ?", cmtMsg.TaskId).Updates(&conversation).Error
-	}
+	return
+	//if err == nil {
+	//
+	//}
 }
