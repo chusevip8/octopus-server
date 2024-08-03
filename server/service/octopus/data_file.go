e7c8f078cc914483c28dc25b3c82efc2e7d3ce08
chusevip8 <chusevip8@gmail.com>
2024-08-03 14:21:39 +0800

    完善功能。

--- server/service/octopus/data_file.go
+++ server/service/octopus/data_file.go
@@ -1,17 +1,26 @@
 package octopus
 
 import (
+	"bufio"
+	"fmt"
 	"github.com/flipped-aurora/gin-vue-admin/server/global"
 	"github.com/flipped-aurora/gin-vue-admin/server/model/example"
 	"github.com/flipped-aurora/gin-vue-admin/server/model/octopus"
 	"github.com/flipped-aurora/gin-vue-admin/server/utils/upload"
 	"mime/multipart"
+	"os"
+	"reflect"
+	"strconv"
 	"strings"
 )
 
 type DataFileService struct{}
 
 func (dataFileService *DataFileService) Upload(setupId string, file example.ExaFileUploadAndDownload) error {
+	err := dataFileService.saveToDB(setupId, file)
+	if err != nil {
+		return err
+	}
 	return global.GVA_DB.Model(&octopus.GenericTaskSetup{}).Where("id = ?", setupId).Updates(map[string]interface{}{
 		"data_file":      file.Name,
 		"data_file_path": file.Url,
@@ -32,4 +41,42 @@ func (dataFileService *DataFileService) UploadFile(setupId string, header *multi
 		Key:  key,
 	}
 	return f, dataFileService.Upload(setupId, f)
+}
+func (dataFileService *DataFileService) saveToDB(setupId string, file example.ExaFileUploadAndDownload) (err error) {
+	txt, err := os.Open(file.Url)
+	if err != nil {
+		return
+	}
+	defer txt.Close()
+	scanner := bufio.NewScanner(txt)
+	for scanner.Scan() {
+		line := scanner.Text()
+		words := strings.Fields(line)
+		bindData := octopus.TaskBindData{}
+		bindData.MainTaskType = "generic"
+		sId, err := strconv.ParseUint(setupId, 10, 0)
+		if err != nil {
+			return err
+		}
+		bindData.TaskSetupId = uint(sId)
+		v := reflect.ValueOf(&bindData).Elem()
+		for i, word := range words {
+			if i >= 10 {
+				break
+			}
+			field := v.FieldByName(fmt.Sprintf("Item%d", i+1))
+			if field.IsValid() && field.CanSet() {
+				field.SetString(word)
+			}
+		}
+		err = taskBindDataServiceApp.CreateTaskBindData(&bindData)
+		if err != nil {
+			return err
+		}
+	}
+	// 检查扫描过程中是否有错误
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+	return nil
 }
